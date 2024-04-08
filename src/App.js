6967d2fb4b279db1b1fe103fe636fb03908c90d5
carlos velasquez <cavelasquez17@gmail.com>
2024-04-08 10:42:12 -0500

    remove home component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import Header from "./components/Header";
 import FeedBackData from "./data/FeedBackData";
-import Home from "./components/Home";
+import FeedbackForm from "./components/FeedbackForm";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackList from "./components/FeedbackList";
 import AboutPage from "./pages/AboutPage";
 
 function App() {
@@ -27,13 +29,17 @@ function App() {
       <div className="container">
         <Routes>
           <Route
+            exact
             path="/"
             element={
-              <Home
-                feedback={feedback}
-                addFeedback={addFeedback}
-                deleteFeedback={deleteFeedback}
-              />
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
             }
           />
           <Route path="/about" element={<AboutPage />} />
